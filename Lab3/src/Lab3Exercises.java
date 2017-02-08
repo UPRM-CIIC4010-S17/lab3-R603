1fb15bcd5aef748578006853d2f6004ff74134aa
4015c16 <4015c16@nispero.ece.uprm.edu>
2017-02-08 12:17:24 -0400

    Added background, border and inner border

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,10 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Ivan Santiago De Jesus c&p");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setLocation(700, 300);
+		myFrame.setSize(500, 500);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
