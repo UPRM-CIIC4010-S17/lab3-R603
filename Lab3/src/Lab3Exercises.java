ad1e648e0b0a7e5ba7c625fae01c9c52d79ae1d1
Ivan <ivan.santiago6@upr.edu>
2017-02-09 20:42:46 -0400

    Added stripes for PR flag

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Ivan Santiago De Jesus");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(700, 300);
-		myFrame.setSize(700, 500);
+		myFrame.setLocation(600, 300);
+		myFrame.setSize(600, 400);
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Polygon;
+import java.awt.Rectangle;
 
 import javax.swing.JPanel;
  
@@ -17,57 +18,29 @@ public class MyPanelClass extends JPanel {
                         //Compute interior coordinates
                         Insets myInsets = this.getInsets();
                         int x1 = myInsets.left;
-                        int y1 = myInsets.top;
+                        int y1 = myInsets.top;	
                         int x2 = getWidth() - myInsets.right - 1;
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
  
                         //Paint the background
-                        g.setColor(Color.BLACK);
+                        g.setColor(Color.WHITE);
                         g.fillRect(x1, y1, width + 1, height + 1);
                         
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);
-                        //Draw red line
+                        //Draw top stripe
                         g.setColor(Color.RED);
-                        g.drawLine(x1, y1, x2, y2);
-                        //Draw red line
-                        g.setColor(Color.RED);
-                        g.drawLine(x1, y1, x2, y2);
-                        
-                        //Draw blue line
-                        g.setColor(Color.BLUE);
-                        g.drawLine(x1, y2, x2, y1);
-                                       //Draw red line
-                        g.setColor(Color.RED);
-                        g.drawLine(x1, y1, x2, y2);
-                        
-                        //Draw blue line
-                        g.setColor(Color.BLUE);
-                        g.drawLine(x1, y2, x2, y1);
+                        g.fillRect(x1, y1, width, 70);
                                        
-                        //Draw blue line
-                        g.setColor(Color.BLUE);
-                        g.drawLine(x1, y2, x2, y1);
-                                       
-                        //Draw second border
-                        g.setColor(Color.WHITE);
-                        g.drawRect(x1 + 5, y1 + 5, width - 10, height - 10);
-                        
-                        //Draw red line
+                        //Draw bottom stripe
                         g.setColor(Color.RED);
-                        g.drawLine(x1, y1, x2, y2);
+                        g.fillRect(x1, getHeight() - 70, width, 70);
                         
-                        //Draw blue line
-                        g.setColor(Color.BLUE);
-                        g.drawLine(x1, y2, x2, y1);
+                        //Draw middle stripe
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, height/2 - 70/2, width, 70);
                         
-//                        //Draw oval
-//                        g.setColor(Color.LIGHT_GRAY);
-//                        g.fillOval(width/2 - 55/2, height/2 - 55/2, 55, 55);
-                        	
+                        //Triangle
                         Polygon p = new Polygon();
                         p.addPoint(x1 + 5, y1 + 25);
                         p.addPoint(x1 + 20, y1 + 10);
@@ -79,20 +52,42 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.YELLOW);
                         g.fillPolygon(p);
 			
+                        //Star
                         Polygon p2 = new Polygon();
+                        //left
                         p2.addPoint(x1 + 25, y1 + 73);
+                        //top
                         p2.addPoint(x1 + 41, y1 + 73);
                         p2.addPoint(x1 + 47, y1 + 58);
                         p2.addPoint(x1 + 53, y1 + 73);
+                        //right
                         p2.addPoint(x1 + 69, y1 + 73);
                         p2.addPoint(x1 + 56, y1 + 83);
                         p2.addPoint(x1 + 61, y1 + 98);
+                        //bottom
                         p2.addPoint(x1 + 47, y1 + 88);
+                        //left
                         p2.addPoint(x1 + 34, y1 + 98);
                         p2.addPoint(x1 + 38, y1 + 83);
-                        g.setColor(Color.WHITE);
-                        g.drawPolygon(p2);
-			
+                        g.setColor(Color.YELLOW);
+                        System.out.println(p2.getBounds());
+                        g.fillPolygon(p2);
+                        
+                        
+                      //Draw red line
+//                      g.setColor(Color.RED);
+//                      g.drawLine(x1, y1, x2, y2);
+//                                     
+//                      //Draw blue line
+//                      g.setColor(Color.BLUE);
+//                      g.drawLine(x1, y2, x2, y1);
+                        
+
+                        
+//                      //Draw oval
+//                      g.setColor(Color.LIGHT_GRAY);
+//                      g.fillOval(width/2 - 55/2, height/2 - 55/2, 55, 55);
+                      	
 			
 			
 			}
